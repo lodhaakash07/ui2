9b04aca0147b0284d798d5ff4f04dfc1e7e087d0
Akash <akash@akashlodha.com>
2018-06-29 18:26:46 +0530

    ux changes

--- src/pages/schedules/schedules.ts
+++ src/pages/schedules/schedules.ts
@@ -5,6 +5,11 @@ import { ScheduleForm } from './schedule-form';
 import { ScheduleApi } from '../../providers/schedule-api';
 import { ScheduleDetails } from './schedule-details';
 import { ScheduleCreate } from './schedule-create.';
+import { ModalController } from 'ionic-angular';
+
+import {
+  CalendarComponentOptions
+} from 'ion2-calendar'
 
 @Component({
   selector: 'schedules',
@@ -15,12 +20,14 @@ export class Schedules {
   schedules: any;
   schedule: any;
   fitness_test_id: any = null;
-  
+  date: string = new Date().toISOString();
+  options: CalendarComponentOptions;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public loadingController: LoadingController,
     public scheduleApi: ScheduleApi,
-    public respUtility: ResponseUtility) {
+    public respUtility: ResponseUtility,
+    public modalCtrl: ModalController) {
 
       if(this.navParams.data["fitness_test_id"] !== null) {
         this.fitness_test_id = this.navParams.data["fitness_test_id"];
@@ -40,13 +47,22 @@ export class Schedules {
     this.scheduleApi.getSchedules(this.fitness_test_id).subscribe(
       schedules => {
         this.schedules = schedules;
+        if(!!this.schedules) {
+          this.options = {
+            from: new Date(this.schedules[0].scheduled_date)
+          }
+        this.schedule = this.schedules[1]
+        }
         console.log("Loaded schedules");
         console.log(schedules);
       },
       error => { this.respUtility.showFailure(error); loader.dismiss(); },
       () => { loader.dismiss(); }
     );
+  }
 
+  onChange($event) {
+    console.log($event)
   }
 
   rateSchedule(schedule) {
